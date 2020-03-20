18a740ebf38b0dabba02915c95d5485f76d69fb8
Ard-Jan Aalberts <a.aalberts@labdigital.nl>
2020-03-20 14:12:10 +0100

    [CP-42] Added support for creating a new role of a space.
    Created Upsert methods with create unit test, took way too long to implement this.

--- role.go
+++ role.go
@@ -1,8 +1,11 @@
 package contentful
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/url"
+	"strconv"
 )
 
 // RolesService service
@@ -10,25 +13,38 @@ type RolesService service
 
 // Role model
 type Role struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Policies    []struct {
-		Effect     string   `json:"effect"`
-		Actions    []string `json:"actions"`
-		Constraint struct {
-			And []struct {
-				Equals []interface{} `json:"equals"`
-			} `json:"and"`
-		} `json:"constraint"`
-	} `json:"policies"`
-	Permissions struct {
-		ContentModel       []string      `json:"ContentModel"`
-		Settings           []interface{} `json:"Settings"`
-		ContentDelivery    []interface{} `json:"ContentDelivery"`
-		Environments       []interface{} `json:"Environments"`
-		EnvironmentAliases []interface{} `json:"EnvironmentAliases"`
-	} `json:"permissions"`
-	Sys *Sys `json:"sys"`
+	Sys         *Sys        `json:"sys"`
+	Name        string      `json:"name"`
+	Description string      `json:"description"`
+	Policies    []Policies  `json:"policies"`
+	Permissions Permissions `json:"permissions"`
+}
+
+type Policies struct {
+	Effect     string     `json:"effect"`
+	Actions    []string   `json:"actions"`
+	Constraint Constraint `json:"constraint"`
+}
+
+type Permissions struct {
+	ContentModel       []string `json:"ContentModel"`
+	Settings           string   `json:"Settings"`
+	ContentDelivery    string   `json:"ContentDelivery"`
+	Environments       string   `json:"Environments"`
+	EnvironmentAliases string   `json:"EnvironmentAliases"`
+}
+
+type Constraint struct {
+	And []ConstraintDetail `json:"and"`
+}
+
+type ConstraintDetail struct {
+	Equals DetailItem `json:"equals"`
+}
+
+type DetailItem struct {
+	Doc      map[string]interface{}
+	ItemType string
 }
 
 // GetVersion returns entity version
@@ -75,4 +91,33 @@ func (service *RolesService) Get(spaceID, roleID string) (*Role, error) {
 	}
 
 	return &role, err
+}
+
+// Upsert updates or creates a new role
+func (service *RolesService) Upsert(spaceID string, r *Role) error {
+	bytesArray, err := json.Marshal(r)
+	fmt.Println(r)
+	if err != nil {
+		return err
+	}
+
+	var path string
+	var method string
+
+	if r.Sys != nil && r.Sys.ID != "" {
+		path = fmt.Sprintf("/spaces/%s/roles/%s", spaceID, r.Sys.ID)
+		method = "PUT"
+	} else {
+		path = fmt.Sprintf("/spaces/%s/roles", spaceID)
+		method = "POST"
+	}
+
+	req, err := service.c.newRequest(method, path, nil, bytes.NewReader(bytesArray))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("X-Contentful-Version", strconv.Itoa(r.GetVersion()))
+
+	return service.c.do(req, r)
 }
--- role_test.go
+++ role_test.go
@@ -1,6 +1,7 @@
 package contentful
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -58,5 +59,74 @@ func TestRolesServiceGet(t *testing.T) {
 	cma.BaseURL = server.URL
 
 	_, err = cma.Roles.Get(spaceID, "0xvkNW6WdQ8JkWlWZ8BC4x")
+	assert.Nil(err)
+}
+
+func TestRolesServiceUpsertCreate(t *testing.T) {
+	var err error
+	assert := assert.New(t)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(r.Method, "POST")
+		assert.Equal(r.RequestURI, "/spaces/"+spaceID+"/roles")
+
+		checkHeaders(r, assert)
+
+		var payload map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&payload)
+		assert.Nil(err)
+
+		name := payload["name"]
+		description := payload["description"]
+		assert.Equal("Author", name)
+		assert.Equal("Describes the author", description)
+
+		w.WriteHeader(200)
+		fmt.Fprintln(w, readTestData("role_1.json"))
+	})
+
+	// test server
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// cma client
+	cma = NewCMA(CMAToken)
+	cma.BaseURL = server.URL
+
+	role := &Role{
+		Name:        "Author",
+		Description: "Describes the author",
+		Policies: []Policies{
+			Policies{
+				Effect: "allow",
+				Actions: []string{
+					"create",
+				},
+				Constraint: Constraint{
+					And: []ConstraintDetail{
+						{
+							Equals: DetailItem{
+								Doc: map[string]interface{}{
+									"doc": "sys.type",
+								},
+								ItemType: "Entry",
+							},
+						},
+					},
+				},
+			},
+		},
+		Permissions: Permissions{
+			ContentModel: []string{
+				"read",
+			},
+			Settings:           "all",
+			ContentDelivery:    "all",
+			Environments:       "all",
+			EnvironmentAliases: "all",
+		},
+	}
+
+	err = cma.Roles.Upsert(spaceID, role)
 	assert.Nil(err)
 }
