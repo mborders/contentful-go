605da5b1e274808a7789ce4b0d8358ccd7a9db15
Ard-Jan Aalberts <a.aalberts@labdigital.nl>
2020-03-26 14:12:40 +0100

    [CP-60], [CP-62], [CP-63] Added support for creating, updating and deleting entry tasks.
    New code added inside the entry tasks service and the unit tests.

--- entry_task.go
+++ entry_task.go
@@ -1,9 +1,12 @@
 package contentful
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
 // EntryTasksService service
@@ -65,4 +68,45 @@ func (service *EntryTasksService) Get(spaceID, entryID, entryTaskID string) (*En
 	}
 
 	return &entryTask, err
+}
+
+// Delete the entry task
+func (service *EntryTasksService) Delete(spaceID, entryID, entryTaskID string) error {
+	path := fmt.Sprintf("/spaces/%s/environments/%s/entries/%s/tasks/%s", spaceID, service.c.Environment, entryID, entryTaskID)
+	method := "DELETE"
+
+	req, err := service.c.newRequest(method, path, nil, nil)
+	if err != nil {
+		return err
+	}
+
+	return service.c.do(req, nil)
+}
+
+// Upsert updates or creates a new entry task
+func (service *EntryTasksService) Upsert(spaceID, entryID string, entryTask *EntryTask) error {
+	bytesArray, err := json.Marshal(entryTask)
+	if err != nil {
+		return err
+	}
+
+	var path string
+	var method string
+
+	if entryTask.Sys != nil && entryTask.Sys.CreatedAt != "" {
+		path = fmt.Sprintf("/spaces/%s/environments/%s/entries/%s/tasks/%s", spaceID, service.c.Environment, entryID, entryTask.Sys.ID)
+		method = "PUT"
+	} else {
+		path = fmt.Sprintf("/spaces/%s/environments/%s/entries/%s/tasks", spaceID, service.c.Environment, entryID)
+		method = "POST"
+	}
+
+	req, err := service.c.newRequest(method, path, nil, bytes.NewReader(bytesArray))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("X-Contentful-Version", strconv.Itoa(entryTask.GetVersion()))
+
+	return service.c.do(req, entryTask)
 }
--- entry_task_test.go
+++ entry_task_test.go
@@ -1,6 +1,7 @@
 package contentful
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -63,4 +64,115 @@ func TestEntryTasksService_Get(t *testing.T) {
 	entryTask, err := cma.EntryTasks.Get(spaceID, "5KsDBWseXY6QegucYAoacS", "RHfHVRz3QkAgcMq4CGg2m5")
 	assertions.Nil(err)
 	assertions.Equal("RHfHVRz3QkAgcMq4CGg2m5", entryTask.Sys.ID)
+}
+
+func TestEntryTasksService_Delete(t *testing.T) {
+	var err error
+	assertions := assert.New(t)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertions.Equal(r.Method, "DELETE")
+		assertions.Equal(r.URL.Path, "/spaces/"+spaceID+"/environments/master/entries/5KsDBWseXY6QegucYAoacS/tasks/RHfHVRz3QkAgcMq4CGg2m5")
+		checkHeaders(r, assertions)
+
+		w.WriteHeader(200)
+	})
+
+	// test server
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// cma client
+	cma = NewCMA(CMAToken)
+	cma.BaseURL = server.URL
+
+	entryTask, err := spaceFromTestData("entry_task_1.json")
+	assertions.Nil(err)
+
+	err = cma.EntryTasks.Delete(spaceID, "5KsDBWseXY6QegucYAoacS", entryTask.Sys.ID)
+	assertions.Nil(err)
+}
+
+func TestEntryTasksService_Upsert_Create(t *testing.T) {
+	assertions := assert.New(t)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertions.Equal(r.Method, "POST")
+		assertions.Equal(r.RequestURI, "/spaces/"+spaceID+"/environments/master/entries/5KsDBWseXY6QegucYAoacS/tasks")
+		checkHeaders(r, assertions)
+
+		var payload map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&payload)
+		assertions.Nil(err)
+		assertions.Equal("new entry task", payload["body"])
+		assertions.Equal("active", payload["status"])
+
+		w.WriteHeader(201)
+		_, _ = fmt.Fprintln(w, string(readTestData("entry_task_new.json")))
+	})
+
+	// test server
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// cma client
+	cma = NewCMA(CMAToken)
+	cma.BaseURL = server.URL
+
+	entryTask := &EntryTask{
+		Body:   "new entry task",
+		Status: "active",
+		AssignedTo: AssignedTo{
+			Sys: Sys{
+				Type:     "Link",
+				LinkType: "User",
+				ID:       "7BslKh9TdKGOK41VmLDjFZ",
+			},
+		},
+	}
+
+	err := cma.EntryTasks.Upsert(spaceID, "5KsDBWseXY6QegucYAoacS", entryTask)
+	assertions.Nil(err)
+
+	assertions.Equal("new entry task", entryTask.Body)
+	assertions.Equal("active", entryTask.Status)
+}
+
+func TestEntryTasksService_Upsert_Update(t *testing.T) {
+	var err error
+	assertions := assert.New(t)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertions.Equal(r.Method, "PUT")
+		assertions.Equal(r.RequestURI, "/spaces/"+spaceID+"/environments/master/entries/5KsDBWseXY6QegucYAoacS/tasks/RHfHVRz3QkAgcMq4CGg2m5")
+		checkHeaders(r, assertions)
+
+		var payload map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&payload)
+		assertions.Nil(err)
+		assertions.Equal("Review translation", payload["body"])
+		assertions.Equal("active", payload["status"])
+
+		w.WriteHeader(200)
+		_, _ = fmt.Fprintln(w, string(readTestData("entry_task_1.json")))
+	})
+
+	// test server
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// cma client
+	cma = NewCMA(CMAToken)
+	cma.BaseURL = server.URL
+
+	entryTask, err := entryTaskFromTestFile("entry_task_new.json")
+	assertions.Nil(err)
+
+	entryTask.Body = "Review translation"
+	entryTask.Status = "active"
+
+	err = cma.EntryTasks.Upsert(spaceID, "5KsDBWseXY6QegucYAoacS", entryTask)
+	assertions.Nil(err)
+	assertions.Equal("Review translation", entryTask.Body)
+	assertions.Equal("active", entryTask.Status)
 }
