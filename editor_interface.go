a3804d30be733de9775fbf829be95f80275ce866
Ard-Jan Aalberts <a.aalberts@labdigital.nl>
2020-03-27 10:05:13 +0100

    [CP-20] Added support for getting single editor interfaces.
    New code added inside the editorInterface service and the corresponding unit test.

--- editor_interface.go
+++ editor_interface.go
@@ -2,6 +2,7 @@ package contentful
 
 import (
 	"fmt"
+	"net/url"
 )
 
 // EditorInterfacesService service
@@ -41,8 +42,8 @@ func (editorInterface *EditorInterface) GetVersion() int {
 }
 
 // List returns an EditorInterface collection
-func (service *EditorInterfacesService) List(spaceID, contentTypeID string) *Collection {
-	path := fmt.Sprintf("/spaces/%s/environments/%s/content_types/%s/editor_interface", spaceID, service.c.Environment, contentTypeID)
+func (service *EditorInterfacesService) List(spaceID string) *Collection {
+	path := fmt.Sprintf("/spaces/%s/environments/%s/editor_interface", spaceID, service.c.Environment)
 
 	req, err := service.c.newRequest("GET", path, nil, nil)
 	if err != nil {
@@ -54,4 +55,23 @@ func (service *EditorInterfacesService) List(spaceID, contentTypeID string) *Col
 	col.req = req
 
 	return col
+}
+
+// Get returns a single EditorInterface
+func (service *EditorInterfacesService) Get(spaceID, contentTypeID string) (*EditorInterface, error) {
+	path := fmt.Sprintf("/spaces/%s/environments/%s/content_types/%s/editor_interface", spaceID, service.c.Environment, contentTypeID)
+	query := url.Values{}
+	method := "GET"
+
+	req, err := service.c.newRequest(method, path, query, nil)
+	if err != nil {
+		return &EditorInterface{}, err
+	}
+
+	var editorInterface EditorInterface
+	if ok := service.c.do(req, &editorInterface); ok != nil {
+		return nil, err
+	}
+
+	return &editorInterface, err
 }
--- editor_interface_test.go
+++ editor_interface_test.go
@@ -15,7 +15,7 @@ func TestEditorInterfacesService_List(t *testing.T) {
 
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		assertions.Equal(r.Method, "GET")
-		assertions.Equal(r.URL.Path, "/spaces/"+spaceID+"/environments/master/content_types/hfM9RCJIk0wIm06WkEOQY/editor_interface")
+		assertions.Equal(r.URL.Path, "/spaces/"+spaceID+"/environments/master/editor_interface")
 
 		checkHeaders(r, assertions)
 
@@ -31,11 +31,39 @@ func TestEditorInterfacesService_List(t *testing.T) {
 	cma = NewCMA(CMAToken)
 	cma.BaseURL = server.URL
 
-	collection, err := cma.EditorInterfaces.List(spaceID, "hfM9RCJIk0wIm06WkEOQY").Next()
+	collection, err := cma.EditorInterfaces.List(spaceID).Next()
 	assertions.Nil(err)
 
 	interfaces := collection.ToEditorInterface()
 	assertions.Equal(1, len(interfaces))
 	assertions.Equal("name", interfaces[0].Controls[0].FieldID)
 	assertions.Equal("extension", interfaces[0].SideBar[0].WidgetNameSpace)
+}
+
+func TestEditorInterfacesService_Get(t *testing.T) {
+	var err error
+	assertions := assert.New(t)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertions.Equal(r.Method, "GET")
+		assertions.Equal(r.URL.Path, "/spaces/"+spaceID+"/environments/master/content_types/hfM9RCJIk0wIm06WkEOQY/editor_interface")
+
+		checkHeaders(r, assertions)
+
+		w.WriteHeader(200)
+		_, _ = fmt.Fprintln(w, readTestData("editor_interface_1.json"))
+	})
+
+	// test server
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// cma client
+	cma = NewCMA(CMAToken)
+	cma.BaseURL = server.URL
+
+	editorInterface, err := cma.EditorInterfaces.Get(spaceID, "hfM9RCJIk0wIm06WkEOQY")
+	assertions.Nil(err)
+	assertions.Equal("name", editorInterface.Controls[0].FieldID)
+	assertions.Equal("extension", editorInterface.SideBar[0].WidgetNameSpace)
 }
