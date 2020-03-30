4f805b7b233768e0e532d7a69ca7493151254f81
Ard-Jan Aalberts <a.aalberts@labdigital.nl>
2020-03-30 10:08:59 +0200

    [CP-23], [CP-24] Added support for creating and updating extensions.
    New code added inside the extensions service and the corresponding test files.

--- extension.go
+++ extension.go
@@ -1,8 +1,11 @@
 package contentful
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/url"
+	"strconv"
 )
 
 // ExtensionsService service
@@ -16,15 +19,25 @@ type Extension struct {
 
 // ExtensionDetails model
 type ExtensionDetails struct {
-	SRC        string      `json:"src"`
-	Name       string      `json:"name"`
-	FieldTypes []FieldType `json:"fieldTypes"`
-	Sidebar    bool        `json:"sidebar"`
+	SRC        string       `json:"src"`
+	Name       string       `json:"name"`
+	FieldTypes []FieldTypes `json:"fieldTypes"`
+	Sidebar    bool         `json:"sidebar"`
 }
 
-// FieldType model
-type FieldType struct {
+// FieldTypes model
+type FieldTypes struct {
 	Type string `json:"type"`
+}
+
+// GetVersion returns entity version
+func (extension *Extension) GetVersion() int {
+	version := 1
+	if extension.Sys != nil {
+		version = extension.Sys.Version
+	}
+
+	return version
 }
 
 // List returns an extensions collection
@@ -60,4 +73,32 @@ func (service *ExtensionsService) Get(spaceID, extensionID string) (*Extension, 
 	}
 
 	return &extension, err
+}
+
+// Upsert updates or creates a new extension
+func (service *ExtensionsService) Upsert(spaceID string, e *Extension) error {
+	bytesArray, err := json.Marshal(e)
+	if err != nil {
+		return err
+	}
+
+	var path string
+	var method string
+
+	if e.Sys != nil && e.Sys.ID != "" {
+		path = fmt.Sprintf("/spaces/%s/environments/%s/extensions/%s", spaceID, service.c.Environment, e.Sys.ID)
+		method = "PUT"
+	} else {
+		path = fmt.Sprintf("/spaces/%s/environments/%s/extensions", spaceID, service.c.Environment)
+		method = "POST"
+	}
+
+	req, err := service.c.newRequest(method, path, nil, bytes.NewReader(bytesArray))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("X-Contentful-Version", strconv.Itoa(e.GetVersion()))
+
+	return service.c.do(req, e)
 }
--- extension_test.go
+++ extension_test.go
@@ -1,6 +1,7 @@
 package contentful
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -65,4 +66,91 @@ func TestExtensionsService_Get(t *testing.T) {
 	extension, err := cma.Extensions.Get(spaceID, "0xvkPW9FdQ1kkWlWZ8ga4x")
 	assertions.Nil(err)
 	assertions.Equal("0xvkPW9FdQ1kkWlWZ8ga4x", extension.Sys.ID)
+}
+
+func TestExtensionsService_Upsert_Create(t *testing.T) {
+	assertions := assert.New(t)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertions.Equal(r.Method, "POST")
+		assertions.Equal(r.RequestURI, "/spaces/"+spaceID+"/environments/master/extensions")
+		checkHeaders(r, assertions)
+
+		var payload map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&payload)
+		assertions.Nil(err)
+		extension := payload["extension"].(map[string]interface{})
+		assertions.Equal("https://example.com/my", extension["src"])
+		assertions.Equal("My awesome extension", extension["name"])
+
+		w.WriteHeader(201)
+		_, _ = fmt.Fprintln(w, string(readTestData("extension_1.json")))
+	})
+
+	// test server
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// cma client
+	cma = NewCMA(CMAToken)
+	cma.BaseURL = server.URL
+
+	extension := &Extension{
+		Extension: ExtensionDetails{
+			SRC:  "https://example.com/my",
+			Name: "My awesome extension",
+			FieldTypes: []FieldTypes{
+				{
+					Type: "Symbol",
+				},
+				{
+					Type: "Text",
+				},
+			},
+			Sidebar: false,
+		},
+	}
+
+	err := cma.Extensions.Upsert(spaceID, extension)
+	assertions.Nil(err)
+	assertions.Equal("https://example.com/my", extension.Extension.SRC)
+	assertions.Equal("My awesome extension", extension.Extension.Name)
+}
+
+func TestExtensionsService_Upsert_Update(t *testing.T) {
+	var err error
+	assertions := assert.New(t)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertions.Equal(r.Method, "PUT")
+		assertions.Equal(r.RequestURI, "/spaces/"+spaceID+"/environments/master/extensions/0xvkPW9FdQ1kkWlWZ8ga4x")
+		checkHeaders(r, assertions)
+
+		var payload map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&payload)
+		assertions.Nil(err)
+		extension := payload["extension"].(map[string]interface{})
+		assertions.Equal("https://example.com/my", extension["src"])
+		assertions.Equal("The updated extension", extension["name"])
+
+		w.WriteHeader(200)
+		_, _ = fmt.Fprintln(w, string(readTestData("extension_updated.json")))
+	})
+
+	// test server
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// cma client
+	cma = NewCMA(CMAToken)
+	cma.BaseURL = server.URL
+
+	extension, err := extensionFromTestFile("extension_1.json")
+	assertions.Nil(err)
+
+	extension.Extension.Name = "The updated extension"
+
+	err = cma.Extensions.Upsert(spaceID, extension)
+	assertions.Nil(err)
+	assertions.Equal("The updated extension", extension.Extension.Name)
 }
