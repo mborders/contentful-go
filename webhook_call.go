3bd3f2b2b2e31c82a0c4317a82145157fc5d5d17
Ard-Jan Aalberts <a.aalberts@labdigital.nl>
2020-03-30 11:34:06 +0200

    [CP-39], [CP-40] Added support for getting call details and health.
    New code added into the webhookCall service and the corresponding test files.

--- webhook_call.go
+++ webhook_call.go
@@ -3,6 +3,7 @@ package contentful
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 // WebhookCallsService service
@@ -11,12 +12,42 @@ type WebhookCallsService service
 // WebhookCall model
 type WebhookCall struct {
 	Sys        *Sys     `json:"sys"`
+	Request    Request  `json:"request,omitempty"`
+	Response   Response `json:"response,omitempty"`
 	StatusCode int      `json:"statusCode"`
 	Errors     []string `json:"errors"`
 	EventType  string   `json:"eventType"`
 	URL        string   `json:"url"`
 	RequestAt  string   `json:"requestAt"`
 	ResponseAt string   `json:"responseAt"`
+}
+
+// Request model
+type Request struct {
+	URL     string            `json:"url"`
+	Method  string            `json:"method"`
+	Headers map[string]string `json:"headers"`
+	Body    string            `json:"body"`
+}
+
+// Response model
+type Response struct {
+	URL        string            `json:"url"`
+	Headers    map[string]string `json:"headers"`
+	Body       string            `json:"body"`
+	StatusCode int               `json:"statusCode"`
+}
+
+// WebhookHealth model
+type WebhookHealth struct {
+	Sys   *Sys          `json:"sys"`
+	Calls HealthDetails `json:"calls"`
+}
+
+// HealthDetails model
+type HealthDetails struct {
+	Total   int `json:"total"`
+	Healthy int `json:"healthy"`
 }
 
 // GetVersion returns entity version
@@ -29,7 +60,7 @@ func (webhookCall *WebhookCall) GetVersion() int {
 	return version
 }
 
-// List returns entries collection
+// List returns a webhook calls collection
 func (service *WebhookCallsService) List(spaceID, webhookID string) *Collection {
 	path := fmt.Sprintf("/spaces/%s/webhooks/%s/calls", spaceID, webhookID)
 
@@ -43,4 +74,42 @@ func (service *WebhookCallsService) List(spaceID, webhookID string) *Collection 
 	col.req = req
 
 	return col
+}
+
+// Get returns details of a single webhook call
+func (service *WebhookCallsService) Get(spaceID, webhookID, callID string) (*WebhookCall, error) {
+	path := fmt.Sprintf("/spaces/%s/webhooks/%s/calls/%s", spaceID, webhookID, callID)
+	query := url.Values{}
+	method := "GET"
+
+	req, err := service.c.newRequest(method, path, query, nil)
+	if err != nil {
+		return &WebhookCall{}, err
+	}
+
+	var webHook WebhookCall
+	if ok := service.c.do(req, &webHook); ok != nil {
+		return nil, err
+	}
+
+	return &webHook, err
+}
+
+// Health returns the health of a webhook
+func (service *WebhookCallsService) Health(spaceID, webhookID string) (*WebhookHealth, error) {
+	path := fmt.Sprintf("/spaces/%s/webhooks/%s/health", spaceID, webhookID)
+	query := url.Values{}
+	method := "GET"
+
+	req, err := service.c.newRequest(method, path, query, nil)
+	if err != nil {
+		return &WebhookHealth{}, err
+	}
+
+	var health WebhookHealth
+	if ok := service.c.do(req, &health); ok != nil {
+		return nil, err
+	}
+
+	return &health, err
 }
