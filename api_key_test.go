e765355960a80e23b6fb8b9d71513b51807a9e4c
Ard-Jan Aalberts <a.aalberts@labdigital.nl>
2020-03-31 14:21:54 +0200

    [CP-182] Adding missing unit tests of the upsert method in APIKeys service.

--- api_key_test.go
+++ api_key_test.go
@@ -1,6 +1,7 @@
 package contentful
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -35,7 +36,7 @@ func TestAPIKeyService_List(t *testing.T) {
 	assertions.Nil(err)
 }
 
-func TestAPIKeyServiceGet(t *testing.T) {
+func TestAPIKeyService_Get(t *testing.T) {
 	var err error
 	assertions := assert.New(t)
 
@@ -59,6 +60,94 @@ func TestAPIKeyServiceGet(t *testing.T) {
 
 	_, err = cma.APIKeys.Get(spaceID, "exampleapikey")
 	assertions.Nil(err)
+}
+
+func TestAPIKeyService_Upsert_Create(t *testing.T) {
+	assertions := assert.New(t)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertions.Equal(r.Method, "POST")
+		assertions.Equal(r.RequestURI, "/spaces/"+spaceID+"/api_keys")
+		checkHeaders(r, assertions)
+
+		var payload map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&payload)
+		assertions.Nil(err)
+		assertions.Equal("Example API Key", payload["name"])
+
+		w.WriteHeader(201)
+		_, _ = fmt.Fprintln(w, string(readTestData("api_key_1.json")))
+	})
+
+	// test server
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// cma client
+	cma = NewCMA(CMAToken)
+	cma.BaseURL = server.URL
+
+	key := &APIKey{
+		Name:        "Example API Key",
+		AccessToken: "b4c0n73n7fu1",
+		Environments: []Environments{
+			{
+				Sys: Sys{
+					ID:       "master",
+					Type:     "Link",
+					LinkType: "Environment",
+				},
+			},
+		},
+		PreviewAPIKey: PreviewAPIKey{
+			Sys: Sys{
+				ID:       "1Mx3FqXX5XCJDtNpVW4BZI",
+				Type:     "Link",
+				LinkType: "PreviewApiKey",
+			},
+		},
+	}
+
+	err := cma.APIKeys.Upsert(spaceID, key)
+	assertions.Nil(err)
+	assertions.Equal("exampleapikey", key.Sys.ID)
+	assertions.Equal("Example API Key", key.Name)
+}
+
+func TestAPIKeyService_Upsert_Update(t *testing.T) {
+	var err error
+	assertions := assert.New(t)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertions.Equal(r.Method, "PUT")
+		assertions.Equal(r.RequestURI, "/spaces/"+spaceID+"/api_keys/exampleapikey")
+		checkHeaders(r, assertions)
+
+		var payload map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&payload)
+		assertions.Nil(err)
+		assertions.Equal("This name is updated", payload["name"])
+
+		w.WriteHeader(200)
+		_, _ = fmt.Fprintln(w, string(readTestData("api_key_updated.json")))
+	})
+
+	// test server
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// cma client
+	cma = NewCMA(CMAToken)
+	cma.BaseURL = server.URL
+
+	key, err := apiKeyFromTestData("api_key_1.json")
+	assertions.Nil(err)
+
+	key.Name = "This name is updated"
+
+	err = cma.APIKeys.Upsert(spaceID, key)
+	assertions.Nil(err)
+	assertions.Equal("This name is updated", key.Name)
 }
 
 func TestAPIKeyServiceDelete(t *testing.T) {
