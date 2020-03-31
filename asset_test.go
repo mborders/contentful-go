e1e4a9f21c677750ef995e6dba37d2ce7da0e5b9
Ard-Jan Aalberts <a.aalberts@labdigital.nl>
2020-03-31 16:59:16 +0200

    [CP-182] Added unit tests for the upsert method of the asset service.

--- asset_test.go
+++ asset_test.go
@@ -1,6 +1,7 @@
 package contentful
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -85,6 +86,103 @@ func TestAssetsService_Get(t *testing.T) {
 
 	_, err = cma.Assets.Get(spaceID, "1x0xpXu4pSGS4OukSyWGUK")
 	assertions.Nil(err)
+}
+
+func TestAssetsService_Upsert_Create(t *testing.T) {
+	assertions := assert.New(t)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertions.Equal(r.Method, "POST")
+		assertions.Equal(r.RequestURI, "/spaces/"+spaceID+"/assets")
+		checkHeaders(r, assertions)
+
+		var payload map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&payload)
+		assertions.Nil(err)
+		fields := payload["fields"].(map[string]interface{})
+		title := fields["title"].(map[string]interface{})
+		assertions.Equal("Doge", title["en-US"])
+
+		w.WriteHeader(201)
+		_, _ = fmt.Fprintln(w, readTestData("asset_1.json"))
+	})
+
+	// test server
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// cma client
+	cma = NewCMA(CMAToken)
+	cma.BaseURL = server.URL
+
+	asset := &Asset{
+		locale: "en-US",
+		Fields: &FileFields{
+			Title:       "Doge",
+			Description: "nice picture",
+			File: &File{
+				Name:        "doge.jpg",
+				ContentType: "image/jpeg",
+				URL:         "//images.contentful.com/cfexampleapi/1x0xpXu4pSGS4OukSyWGUK/cc1239c6385428ef26f4180190532818/doge.jpg",
+				UploadURL:   "",
+				Detail: &FileDetail{
+					Size: 522943,
+					Image: &FileImage{
+						Width:  5800,
+						Height: 4350,
+					},
+				},
+			},
+		},
+	}
+
+	err := cma.Assets.Upsert(spaceID, asset)
+	assertions.Nil(err)
+	assertions.Equal("Doge", asset.Fields.Title)
+	assertions.Equal("doge.jpg", asset.Fields.File.Name)
+}
+
+func TestAssetsService_Upsert_Update(t *testing.T) {
+	var err error
+	assertions := assert.New(t)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertions.Equal(r.Method, "PUT")
+		assertions.Equal(r.RequestURI, "/spaces/"+spaceID+"/assets/1x0xpXu4pSGS4OukSyWGUK")
+		checkHeaders(r, assertions)
+
+		var payload map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&payload)
+		assertions.Nil(err)
+		fields := payload["fields"].(map[string]interface{})
+		title := fields["title"].(map[string]interface{})
+		description := fields["description"].(map[string]interface{})
+		assertions.Equal("Updated", title["en-US"])
+		assertions.Equal("Lorum Ipsum", description["en-US"])
+
+		w.WriteHeader(200)
+		_, _ = fmt.Fprintln(w, readTestData("asset_updated.json"))
+	})
+
+	// test server
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// cma client
+	cma = NewCMA(CMAToken)
+	cma.BaseURL = server.URL
+
+	asset, err := assetFromTestData("asset_1.json")
+	assertions.Nil(err)
+
+	asset.Fields.Title = "Updated"
+	asset.Fields.Description = "Lorum Ipsum"
+	asset.locale = "en-US"
+
+	err = cma.Assets.Upsert(spaceID, asset)
+	assertions.Nil(err)
+	assertions.Equal("Updated", asset.Fields.Title)
+	assertions.Equal("Lorum Ipsum", asset.Fields.Description)
 }
 
 func TestAssetsService_Delete(t *testing.T) {
