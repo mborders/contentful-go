06e6969292d492b1587568601c940050ac8d2365
Ard-Jan Aalberts <a.aalberts@labdigital.nl>
2020-04-06 10:39:49 +0200

    [CP-182] Added extra unit test.
    Added am extra unit test for the delete method in the resource service.

--- resource_test.go
+++ resource_test.go
@@ -110,3 +110,31 @@ func TestResourcesService_Delete(t *testing.T) {
 	err = urc.Resources.Delete(spaceID, resource.Sys.ID)
 	assertions.Nil(err)
 }
+
+func TestResourcesService_Delete_2(t *testing.T) {
+	var err error
+	assertions := assert.New(t)
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertions.Equal(r.Method, "DELETE")
+		assertions.Equal(r.RequestURI, "/spaces/"+spaceID+"/uploads/2DNvIbYNELgqLJUkgTeIOV")
+
+		w.WriteHeader(400)
+	})
+
+	// test server
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	// cma client
+	urc = NewResourceClient(CMAToken)
+	urc.BaseURL = server.URL
+
+	// test role
+	resource, err := resourceFromTestFile("resource_1.json")
+	assertions.Nil(err)
+
+	// delete role
+	err = urc.Resources.Delete(spaceID, resource.Sys.ID)
+	assertions.NotNil(err)
+}
